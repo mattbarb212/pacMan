27d537afedd3ce97d9cac4afc6e4cbc234777090
Matthew Barber <mattbarb212@outlook.com>
2021-03-24 14:42:52 -0400

    Add project files.

---
+++ pacMan/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace pacMan
+{
+    partial class pacMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.controlsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Pioneer BT", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Gold;
+            this.titleLabel.Location = new System.Drawing.Point(103, 36);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(282, 74);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "PacMan";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Black;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.playButton.FlatAppearance.BorderSize = 0;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Pioneer BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.Gold;
+            this.playButton.Location = new System.Drawing.Point(153, 184);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(167, 27);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // controlsButton
+            // 
+            this.controlsButton.BackColor = System.Drawing.Color.Black;
+            this.controlsButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.controlsButton.FlatAppearance.BorderSize = 0;
+            this.controlsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.controlsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.controlsButton.Font = new System.Drawing.Font("Pioneer BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.controlsButton.ForeColor = System.Drawing.Color.Gold;
+            this.controlsButton.Location = new System.Drawing.Point(153, 255);
+            this.controlsButton.Name = "controlsButton";
+            this.controlsButton.Size = new System.Drawing.Size(167, 27);
+            this.controlsButton.TabIndex = 2;
+            this.controlsButton.Text = "Controls";
+            this.controlsButton.UseVisualStyleBackColor = false;
+            // 
+            // pacMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.controlsButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "pacMan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PacMan";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PacMan_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PacMan_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.PacMan_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button controlsButton;
+    }
+}
+
---
+++ pacMan/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pacMan
+{
+    public partial class pacMan : Form
+    {
+        #region Key Binds
+        bool upDown = false;
+        bool downDown = false;
+        bool leftDown = false;
+        bool rightDown = false;
+        bool spaceBar = false;
+        #endregion
+
+        string gameState = "waiting";
+
+        SolidBrush goldBrush = new SolidBrush(Color.Gold);
+        Pen bluePen = new Pen(Color.Blue);
+
+        public pacMan()
+        {
+            InitializeComponent();
+        }
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            titleLabel.Text = "";
+            playButton.Visible = false;
+            controlsButton.Visible = false;
+            gameTimer.Enabled = true;
+            gameState = "running";
+        }        
+        private void PacMan_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upDown = true;
+                    break;
+                case Keys.Down:
+                    downDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Space:
+                    spaceBar = true;
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void PacMan_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upDown = false;
+                    break;
+                case Keys.Down:
+                    downDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+            }
+        }
+
+        private void PacMan_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
