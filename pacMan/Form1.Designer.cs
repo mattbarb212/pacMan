e2ff1c9f234e419195630b81ba8f6aa2ba00d108
Matthew Barber <mattbarb212@outlook.com>
2022-06-16 14:00:54 -0400

    h

--- pacMan/Form1.Designer.cs
+++ pacMan/Form1.Designer.cs
@@ -49,11 +49,12 @@
             // titleLabel
             // 
             this.titleLabel.BackColor = System.Drawing.Color.Transparent;
-            this.titleLabel.Font = new System.Drawing.Font("Pioneer BT", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLabel.ForeColor = System.Drawing.Color.Gold;
-            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Location = new System.Drawing.Point(18, 14);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(476, 74);
+            this.titleLabel.Size = new System.Drawing.Size(714, 114);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "PacMan";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -65,11 +66,12 @@
             this.playButton.FlatAppearance.BorderSize = 0;
             this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.playButton.Font = new System.Drawing.Font("Pioneer BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.playButton.ForeColor = System.Drawing.Color.Gold;
-            this.playButton.Location = new System.Drawing.Point(169, 131);
+            this.playButton.Location = new System.Drawing.Point(254, 201);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(167, 27);
+            this.playButton.Size = new System.Drawing.Size(250, 42);
             this.playButton.TabIndex = 1;
             this.playButton.Text = "Play Game";
             this.playButton.UseVisualStyleBackColor = false;
@@ -83,11 +85,12 @@
             this.controlsButton.FlatAppearance.BorderSize = 0;
             this.controlsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.controlsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.controlsButton.Font = new System.Drawing.Font("Pioneer BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.controlsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.controlsButton.ForeColor = System.Drawing.Color.Gold;
-            this.controlsButton.Location = new System.Drawing.Point(169, 213);
+            this.controlsButton.Location = new System.Drawing.Point(254, 328);
+            this.controlsButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.controlsButton.Name = "controlsButton";
-            this.controlsButton.Size = new System.Drawing.Size(167, 27);
+            this.controlsButton.Size = new System.Drawing.Size(250, 42);
             this.controlsButton.TabIndex = 2;
             this.controlsButton.Text = "Controls";
             this.controlsButton.UseVisualStyleBackColor = false;
@@ -98,32 +101,35 @@
             // 
             this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
             this.scoreLabel.Enabled = false;
-            this.scoreLabel.Font = new System.Drawing.Font("Pioneer BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scoreLabel.ForeColor = System.Drawing.Color.Blue;
-            this.scoreLabel.Location = new System.Drawing.Point(376, 83);
+            this.scoreLabel.Location = new System.Drawing.Point(564, 128);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scoreLabel.Name = "scoreLabel";
-            this.scoreLabel.Size = new System.Drawing.Size(112, 85);
+            this.scoreLabel.Size = new System.Drawing.Size(168, 131);
             this.scoreLabel.TabIndex = 3;
             // 
             // livesLabel
             // 
             this.livesLabel.BackColor = System.Drawing.Color.Transparent;
             this.livesLabel.Enabled = false;
-            this.livesLabel.Font = new System.Drawing.Font("Pioneer BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.livesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.livesLabel.ForeColor = System.Drawing.Color.Red;
-            this.livesLabel.Location = new System.Drawing.Point(402, 157);
+            this.livesLabel.Location = new System.Drawing.Point(603, 242);
+            this.livesLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.livesLabel.Name = "livesLabel";
-            this.livesLabel.Size = new System.Drawing.Size(70, 49);
+            this.livesLabel.Size = new System.Drawing.Size(105, 75);
             this.livesLabel.TabIndex = 4;
             // 
             // controlsLabel
             // 
             this.controlsLabel.BackColor = System.Drawing.Color.Transparent;
-            this.controlsLabel.Font = new System.Drawing.Font("Pioneer BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.controlsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.controlsLabel.ForeColor = System.Drawing.Color.White;
-            this.controlsLabel.Location = new System.Drawing.Point(12, 83);
+            this.controlsLabel.Location = new System.Drawing.Point(18, 128);
+            this.controlsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.controlsLabel.Name = "controlsLabel";
-            this.controlsLabel.Size = new System.Drawing.Size(476, 320);
+            this.controlsLabel.Size = new System.Drawing.Size(714, 492);
             this.controlsLabel.TabIndex = 5;
             this.controlsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.controlsLabel.Visible = false;
@@ -136,11 +142,12 @@
             this.backButton.FlatAppearance.BorderSize = 0;
             this.backButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
             this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.backButton.Font = new System.Drawing.Font("Pioneer BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.backButton.ForeColor = System.Drawing.Color.Gold;
-            this.backButton.Location = new System.Drawing.Point(169, 316);
+            this.backButton.Location = new System.Drawing.Point(254, 486);
+            this.backButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(167, 27);
+            this.backButton.Size = new System.Drawing.Size(250, 42);
             this.backButton.TabIndex = 6;
             this.backButton.Text = "Back";
             this.backButton.UseVisualStyleBackColor = false;
@@ -151,19 +158,20 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::pacMan.Properties.Resources.PacManRightMouthOpen;
-            this.pictureBox1.Location = new System.Drawing.Point(44, 69);
+            this.pictureBox1.Location = new System.Drawing.Point(66, 106);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(444, 406);
+            this.pictureBox1.Size = new System.Drawing.Size(666, 625);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
             // 
             // pacMan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.ClientSize = new System.Drawing.Size(750, 769);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.livesLabel);
             this.Controls.Add(this.controlsButton);
@@ -174,6 +182,7 @@
             this.Controls.Add(this.scoreLabel);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "pacMan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PacMan";
